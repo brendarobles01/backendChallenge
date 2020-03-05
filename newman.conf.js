611f7e6c795f83fd1a174400c88556a69f7e6ea9
Brenda <39286339+brendarobles01@users.noreply.github.com>
2020-03-05 11:31:24 -0600

    Update newman.conf.js

--- newman.conf.js
+++ newman.conf.js
@@ -3,13 +3,7 @@ const newman = require('newman');
 newman.run({
     collection: require('../BrendaR_GraphqlWorkshop/postmanJson/BrendaRobles_GraphqlWorkshop.postman_collection.json'),
     exportEnvironment: require('../BrendaR_GraphqlWorkshop/postmanJson/url.postman_environment.json'),
-    reporters: 'html',
-    reporter: {
-        html: {
-            export: './htmlResults.html', 
-            template: './customTemplate.hbs'
-        }
-    }
+    reporters: 'cli'
 }, function (err) {
     if (err) { throw err; }
     console.log('collection run complete!');
